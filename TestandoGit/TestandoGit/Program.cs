0929046084a3d59dbc2c224b92c7db132ec76bf3
Davi <daviratchet@hotmail.com>
2019-03-31 21:59:57 -0300

    Um novo projeto foi adicionado

---
+++ TestandoGit/TestandoGit/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace TestandoGit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Olá mundo!");
+            Console.WriteLine("Estou aprendendo git!!!");
+        }
+    }
+}

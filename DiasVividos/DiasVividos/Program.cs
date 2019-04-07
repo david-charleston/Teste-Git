6b99119265d6ec339d30ac3a29ed4f9ecf563531
Davi <daviratchet@hotmail.com>
2019-04-07 14:27:46 -0300

    Adicionado um Projeto que calcula os dias vividos de uma pessoa

---
+++ DiasVividos/DiasVividos/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace DiasVividos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime dataAtual = DateTime.Now;
+            DateTime dataNasc;
+            TimeSpan result;
+            int ano, mes, dia;
+
+            Console.Write("Informe o seu ano de nascimento: ");
+            ano = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Informe o seu mês de nascimento: ");
+            mes = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Informe o seu dia de nascimento: ");
+            dia = Convert.ToInt32(Console.ReadLine());
+
+            dataNasc = new DateTime(ano, mes, dia);
+
+            result = dataAtual.Subtract(dataNasc);
+
+            Console.ReadKey();
+        }
+    }
+}

a9a69a2f9474abdb9ceadcfa6df21e55a7316bc8
Davi <daviratchet@hotmail.com>
2019-04-07 15:09:14 -0300

    Projeto que transforma os segundos em horas, minutos e segundos

---
+++ CalcularHorasMinSegundos/CalcularHorasMinSegundos/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace CalcularHorasMinSegundos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int tempoSeg, h, m, s;
+
+            Console.Write("Informe a duração do evento em segundos: ");
+            tempoSeg = Convert.ToInt32(Console.ReadLine());
+            h = tempoSeg / 3600;
+            m = (tempoSeg - (h * 3600));
+            s = tempoSeg - (h * 3600) - (m * 60);
+
+            TimeSpan duracao = new TimeSpan(h, m, s);
+
+            Console.WriteLine($"A duração do evento é de {duracao}");
+
+            Console.WriteLine();
+            Console.Write("Pressione qualquer tecla para continuar...");
+            Console.ReadLine();
+        }
+    }
+}

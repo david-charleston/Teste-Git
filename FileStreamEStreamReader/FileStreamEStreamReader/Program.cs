1403d69ed2ffa46f0d67d22ed3f490112683d15e
Davi <daviratchet@hotmail.com>
2019-05-23 22:34:17 -0300

    Trabalhando com o FileStream e StreamReader

--- FileStreamEStreamReader/FileStreamEStreamReader/Program.cs
+++ FileStreamEStreamReader/FileStreamEStreamReader/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace FileStreamEStreamReader
 {
@@ -6,7 +7,36 @@ namespace FileStreamEStreamReader
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Trabalhando com FileStream e StreamReader
+            string path = @"C:\Users\davir\Desktop\File.txt";
+            FileStream fileStream = null;
+            StreamReader streamReader = null;
+
+            try
+            {
+                fileStream = new FileStream(path, FileMode.Open);
+                streamReader = new StreamReader(fileStream);
+
+                // Lê apenas uma linha do arquivo
+                string line = streamReader.ReadToEnd();
+
+                Console.WriteLine(line);
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine($"Error: {e.Message}");
+            }
+            finally
+            {
+                if (streamReader != null)
+                {
+                    streamReader.Close();
+                }
+                if (fileStream != null)
+                {
+                    fileStream.Close();
+                }
+            }
         }
     }
 }

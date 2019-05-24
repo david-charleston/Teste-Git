9ee1bf417ceafec472bd8a1f850ef250d0b00e72
Davi <daviratchet@hotmail.com>
2019-05-23 23:48:31 -0300

    Exemplo básico com o Path

---
+++ ExemploPath/ExemploPath/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+
+namespace ExemploPath
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\Users\davir\Desktop\Pasta_Teste\File.txt";
+
+            Console.WriteLine($"DirectorySeparatorChar: {Path.DirectorySeparatorChar}");
+            Console.WriteLine($"GetDirectoryName: {Path.GetDirectoryName(path)}");
+            Console.WriteLine($"PathSeparator: {Path.PathSeparator}");
+            Console.WriteLine($"GetFileName: {Path.GetFileName(path)}");
+            Console.WriteLine($"GetFileNameWithoutExtension: {Path.GetFileNameWithoutExtension(path)}");
+            Console.WriteLine($"GetExtension: {Path.GetExtension(path)}");
+            Console.WriteLine($"GetFullPath: {Path.GetFullPath(path)}");
+            Console.WriteLine($"GetTempPath: {Path.GetTempPath()}");
+        }
+    }
+}

a61df67e54030a6c1ca34349681397977e328269
Davi <daviratchet@hotmail.com>
2019-05-23 22:46:28 -0300

    Utilizando o bloco using

---
+++ BlockUsing/BlockUsing/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+
+namespace BlockUsing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\Users\davir\Desktop\File.txt";
+
+            try
+            {
+                using (FileStream fs = new FileStream(path, FileMode.Open))
+                {
+                    using (StreamReader sr = new StreamReader(fs))
+                    {
+                        Console.WriteLine($"Conteúdo do arquivo {Path.GetFileName(path)}");
+                        Console.WriteLine();
+
+                        while (!sr.EndOfStream)
+                        {
+                            string line = sr.ReadLine();
+                            Console.WriteLine(line);
+                        }
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}

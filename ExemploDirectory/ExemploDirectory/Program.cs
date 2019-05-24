613151a5f1af7f555015e6d313217d0a9fab854a
Davi <daviratchet@hotmail.com>
2019-05-23 23:34:22 -0300

    Exemplo básico com o Directory

---
+++ ExemploDirectory/ExemploDirectory/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace ExemploDirectory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\Users\davir\Desktop\Pasta_Teste";
+
+            try
+            {
+                // retorna todas as pastas que existe do diretório de origem
+                IEnumerable<string> folders = Directory.EnumerateDirectories(path, "*.*", 
+                    SearchOption.AllDirectories);
+
+                foreach (string folder in folders)
+                {
+                    Console.WriteLine(folder);
+                }
+
+                Console.WriteLine();
+
+                // retorna todos os arquivos do diretório de origem
+                IEnumerable<string> files = Directory.EnumerateFiles(path, "*.*", SearchOption.AllDirectories);
+                foreach (string file in files)
+                {
+                    Console.WriteLine(file);
+                }
+
+                // Cria uma pasta dentro do diretório
+                Directory.CreateDirectory($"{path}\\newFolder");
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine($"Error: {e.Message}");
+            }
+        }
+    }
+}

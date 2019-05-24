bcd0172729ea1ba6edcbb899c81a29e391ded3f1
Davi <daviratchet@hotmail.com>
2019-05-23 22:59:30 -0300

    Exemplo básico do StreamWriter

---
+++ ExemploStreamWriter/ExemploStreamWriter/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+
+namespace ExemploStreamWriter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sourcePath = @"C:\Users\davir\Desktop\File.txt";
+            string targetPath = @"c:\Users\davir\Desktop\File1.txt";
+
+            try
+            {
+                string[] lines = File.ReadAllLines(sourcePath);
+
+                using (StreamWriter sw = File.AppendText(targetPath))
+                {
+                    foreach (string line in lines)
+                    {
+                        sw.WriteLine(line.ToUpper());
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine($"Error: {e.Message}");
+            }
+        }
+    }
+}

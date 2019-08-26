aecdc7f6eb4c779ecce2886876bce1559b1014df
Davi <daviratchet@hotmail.com>
2019-08-25 22:02:10 -0300

    Exemplo utilizando o IComparable finalizado

--- ExemploComparison/ExemploComparison/Program.cs
+++ ExemploComparison/ExemploComparison/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using ExemploComparison.Entities;
 
 namespace ExemploComparison
 {
@@ -6,7 +8,22 @@ namespace ExemploComparison
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<Product> list = new List<Product>
+            {
+                new Product("TV", 1250),
+                new Product("Notebook", 2755),
+                new Product("Armário", 575),
+                new Product("Celular", 1275)
+            };
+
+            list.Sort();
+
+            foreach (Product product in list)
+            {
+                Console.WriteLine(product);
+            }
+
+            Console.ReadKey();
         }
     }
 }

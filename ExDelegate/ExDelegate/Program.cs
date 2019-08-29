51a8b30c03367b0dea2f6dd82706bd78d51f67ac
Davi <daviratchet@hotmail.com>
2019-08-28 23:35:35 -0300

    Exemplo de Delegate

---
+++ ExDelegate/ExDelegate/Program.cs
@@ -0,0 +1,25 @@
+﻿using ExDelegate.Services;
+
+namespace ExDelegate
+{
+    // Definição de delegate
+    // O Delegate é um type safety
+    // O Delegate pode receber uma função como parâmetros
+    delegate double BinaryNumericOperation(double n1, double n2);
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = 10;
+            double b = 12;
+
+            // Ele faz referência a este método
+            BinaryNumericOperation op = CalculationService.Sum;
+            // O método foi chamado através do delegate
+            double result = op(a, b);
+
+            System.Console.WriteLine(result);
+        }
+    }
+}

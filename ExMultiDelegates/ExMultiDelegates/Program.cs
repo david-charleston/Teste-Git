c58117edf7a5da0b2970f5882448e0dee1546934
Davi <daviratchet@hotmail.com>
2019-08-28 23:55:54 -0300

    Exemplo simples de multi delegates

---
+++ ExMultiDelegates/ExMultiDelegates/Program.cs
@@ -0,0 +1,24 @@
+﻿using ExMultiDelegates.Services;
+
+namespace ExMultiDelegates
+{
+    delegate void CalculationOperation(double x, double y);
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = 55;
+            double b = 50;
+
+            /**
+             *  Multi delegate executa as funções que ele faz referência
+             *  na ordem que lhe foram atribuidas
+             */
+            CalculationOperation op = CalculationService.ShowMax;
+            op += CalculationService.ShowSum;
+
+            op.Invoke(a, b);
+        }
+    }
+}

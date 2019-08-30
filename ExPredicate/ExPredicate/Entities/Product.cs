4d3cff4769c5196e41465cb7feac7e346883e996
Davi <daviratchet@hotmail.com>
2019-08-30 00:08:54 -0300

    Primeiro exemplo do predicate utilizando a expressão lambda

---
+++ ExPredicate/ExPredicate/Entities/Product.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExPredicate.Entities
+{
+    class Product
+    {
+        public string Name { get; private set; }
+        public double Price { get; private set; }
+
+        public Product(string name, double price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+        public override string ToString()
+        {
+            return $"{Name}, {string.Format("{0:c2}", Price)}";
+        }
+    }
+}
---
+++ ExPredicate/ExPredicate/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using ExPredicate.Entities;
+using System.Collections.Generic;
+
+namespace ExPredicate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Product> products = new List<Product>
+            {
+                new Product("TV", 900.00),
+                new Product("Mouse", 50.00),
+                new Product("Tablet", 350.00),
+                new Product("HD Case", 80.90)
+            };
+
+            foreach (Product product in products)
+            {
+                Console.WriteLine(product);
+            }
+
+            Console.WriteLine();
+
+            Console.WriteLine("Removendo os produtos com o valor acima de R$ 100");
+            products.RemoveAll(p => p.Price >= 100.00);
+
+            foreach (Product p in products)
+            {
+                Console.WriteLine(p);
+            }
+        }
+    }
+}
